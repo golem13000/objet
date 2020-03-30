f22203567a80e7408ec3442d19d71d2e8d800cb6
golem13000 <golem13000@hotmail.fr>
2020-03-30 10:48:12 +0200

    objet

--- index.php
+++ index.php
@@ -8,8 +8,8 @@ public $level;
 public $hp;
 public $vie;
 
-function caracteristique() {
-    echo $this->nom ." a une force de ". $this->force;
+public function caracteristique() {
+    echo "le personnage $this->nom a une force de $this->force pour un level $this->level";
 
 }
 function etat() {
@@ -31,21 +31,30 @@ $perso3 = new personnage();
 $perso1->nom = "Rose";
 $perso1->force = 12;
 $perso1->santé = 100;
+$perso1->hp = 10;
+$perso1->vie = 3;
 
 
 $perso2->nom = "Golbu";
 $perso2->force = 15;
 $perso2->santé =100;
+$perso2->hp = 5;
+$perso2->vie = 1;
 
 $perso3->nom = "Arthis";
 $perso3->force = 13;
 $perso3->santé = 100;
+$perso3->hp = 0;
+$perso3->vie = 0;
+
+$perso1->$caracteristique;
 
 
 
 
 
 
+var_dump($perso2);
 
 
 
