7a437023ea00402349f65cb6157fe2fec3c4d6fe
golem13000 <golem13000@hotmail.fr>
2020-03-30 14:40:54 +0200

    objet1

--- index.php
+++ index.php
@@ -2,18 +2,27 @@
 
 class Personnage {
 
-public $nom;
-public $force;
-public $level;
-public $hp;
-public $vie;
+private $nom;
+private $force;
+private $level;
+private $mort;
+private $vie;
+
+
+function __construct(string $nom, int $force, int $level = 1) {
+    $this-> nom = $nom;
+    $sthis->force = $force;
+    $this->level  = $level;
+}
+
 
 public function caracteristique() {
-    echo "le personnage $this->nom a une force de $this->force pour un level $this->level";
+    $etat = ($this->mort)? "mort" : "vivant";
+    echo $this->nom." a une force de " .$this->force. "pour un level" .$this->level." son etat de santé est de ".$this->vie."points/100, notre perso est donc " .$etat; 
 
 }
 function etat() {
-    if($this->hp >= 1) {
+    if($this->vie < = 1) {
         $vie = "en vie";
     } else {
         $vie = "est mort";
@@ -22,39 +31,50 @@ function etat() {
     return $vie;
 }
 
+function getNom(): string {
+    return $this->nom;
 }
 
-$perso1 = new Personnage();
-$perso2 = new personnage();
-$perso3 = new personnage();
+function setNom(string $nom) {
+    $this->nom = $nom;
+
+
+
+}
+
+}
+
+$perso1 = new Personnage("Rose",12);
+$perso2 = new personnage("Golbu",15, 1);
+$perso3 = new personnage("Arthis",13,1);
+
+echo $perso1->getNom();
+
+$perso2->setNom("ralf");
+echo $perso2->gerNom();
+
 
 $perso1->nom = "Rose";
 $perso1->force = 12;
-$perso1->santé = 100;
-$perso1->hp = 10;
-$perso1->vie = 3;
+$perso1->level = 1;
+$perso1->mort = false;
+$perso1->vie = 100;
 
 
 $perso2->nom = "Golbu";
 $perso2->force = 15;
-$perso2->santé =100;
-$perso2->hp = 5;
+$perso2->level =1;
+$perso2->mort = false;
 $perso2->vie = 1;
 
 $perso3->nom = "Arthis";
 $perso3->force = 13;
-$perso3->santé = 100;
-$perso3->hp = 0;
+$perso3->level = 1;
+$perso3->mort = true;
 $perso3->vie = 0;
 
-$perso1->$caracteristique;
+//$perso1->caracteristiques();
 
-
-
-
-
-
-var_dump($perso2);
 
 
 
